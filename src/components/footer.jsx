23fc0996e2563fd9d1beb23fa080b4c5d336c043
Mugenx <sahawu.mail@gmail.com>
2019-01-14 18:47:55 -0500

    beta-1

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,12 +4,7 @@ class Footer extends Component {
   render() {
     return (
       <div className="text-center footer-text mt-5">
-        <div className="d-inline-block">
-          <a href="../../public/sample.csv" className="sampleFile" download>
-            Download Sample
-          </a>
-        </div>
-        <div className="d-inline-block copy-right">
+        <div className="copy-right">
           © {new Date().getFullYear()} &nbsp;
           <a href="https://mugenx.io">MUGENX.IO</a>
         </div>
